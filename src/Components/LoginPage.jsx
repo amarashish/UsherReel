40b20650f9a47a4abb7bd893b89c0c49a2dbf015
Amar Ashish <amarashish000@gmail.com>
2024-09-27 12:45:34 +0530

    formatting

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -19,10 +19,13 @@ const LoginPage = ({ toggleForm }) => {
 
   return (
     <div className='loginDialogue'>
-      <div style={{alignSelf:'flex-start', margin:'10px'}}>App Name</div>
+      <div style={{display:'flex', alignSelf:'flex-start', margin:'10px'}}>
+        <img src='https://i.ibb.co/1XDPqtf/usher.png' style={{height:'35px', width:'35px', margin:'16px 10px 0 10px'}}/>
+        <h2>Ushereel</h2>
+        </div>
       <form onSubmit={handleLoginSubmit}>
         <div>
-          <input placeholder="Email/Phone no." style={{ borderRadius: '15px', margin: '10px', padding:'8px' }}
+          <input placeholder="Email/Phone no." style={{ borderRadius: '15px', margin: '10px', padding:'10px 15px' }}
             type="email"
             name="email"
             value={loginForm.email}
@@ -30,24 +33,24 @@ const LoginPage = ({ toggleForm }) => {
           />
         </div>
         <div>
-          <input placeholder="Password" style={{ borderRadius: '15px', margin: '10px', padding:'8px' }}
+          <input placeholder="Password" style={{ borderRadius: '15px', margin: '10px', padding:'10px 15px' }}
             type="password"
             name="password"
             value={loginForm.password}
             onChange={handleLoginChange}
           />
-          <div style={{padding:'10px'}}>
+          <div style={{padding:'10px', marginRight:'63px'}}>
             <input type="checkbox" id="checkboxId" name="checkboxName" value="checkboxValue" ></input>
-            <label for="checkboxId" style={{marginLeft:'10px'}}>Remember me</label>
+            <label for="checkboxId" style={{marginLeft:'10px', fontSize:'15px'}}>Remember me</label>
           </div>
-          <p>By clicking agree & join or continue you agree to the Ushereel <br /> <a href='' target='_black'>User Agreement.</a> <a href='' target='_black'>Privacy Policy.</a> and <a href='' target='_black'> Cookie Policy</a></p>
+          <p style={{fontSize:'16px'}}>By clicking agree & join or continue you agree to the Ushereel <br /> <a href='' target='_black'>User Agreement.</a> <a href='' target='_black'>Privacy Policy.</a> and <a href='' target='_black'> Cookie Policy</a></p>
         </div>
-        <button style={{ borderRadius: '20px', backgroundColor: 'blue', color: 'black', padding:'15px'}} type="submit"><b>Agree & Join</b></button>
+        <button style={{margin:'10px', borderRadius: '20px', backgroundColor: '#315cfd', color: 'white', padding:'15px 30px', fontSize:'17px'}} type="submit"><b>Agree & Join</b></button>
       </form>
       <div style={{ display: 'flex', margin:'10px', alignItems:'center' }}>
-        <hr style={{ width: '30rem', border: ' 1.2px solid black' }} />
+        <hr style={{ width: '35em', border: ' 1.2px solid black' }} />
         <h2>or</h2>
-        <hr style={{ width: '30rem', border: '1.2px solid black' }} />
+        <hr style={{ width: '35em', border: '1.2px solid black' }} />
       </div>
       <div>
         <img src='https://i.ibb.co/ZH9XSq0/fb.jpg' style={{height:'50px', widows:'50px', margin:'8px'}} 
